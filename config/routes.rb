4eceab1280f7cec21e63f64a8885a658903a1638
Justincadburywong <justincadburywong@gmail.com>
2018-04-07 10:46:59 -0700

    add routes and associations

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   devise_for :users
+  # resources :users do
   resources :pictures, only: [:index, :create]
+  # end
+  
   root 'pictures#index'
 end
--- db/migrate/20180407005900_create_pictures.rb
+++ db/migrate/20180407005900_create_pictures.rb
@@ -2,6 +2,8 @@ class CreatePictures < ActiveRecord::Migration[5.1]
   def change
     create_table :pictures do |t|
       t.string :file_data
+      t.string :tags, array: true
+      # t.integer :user_id
 
       t.timestamps
     end
