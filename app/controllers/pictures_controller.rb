d69f2a81b24c8b38a76adc62e8becfeb4478b8c8
Justincadburywong <justincadburywong@gmail.com>
2018-04-06 18:22:26 -0700

    update js and bootstrap requirements

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,7 +2,7 @@ class PicturesController < ApplicationController
   # skip_before_action :verify_authenticity_token, only: [:create]
 
   def index
-    @pictures = Picture.sorted
+    @pictures = Picture.all
   end
 
   def create
