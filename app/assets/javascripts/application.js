07442139d01666ec4586512d8b074e4b6549e57f
Justincadburywong <justincadburywong@gmail.com>
2018-04-12 00:52:36 -0700

    add slideshow and search function

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,42 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree ./features
+
+
+$(document).ready(function(){
+  eventListeners();
+})
+
+function eventListeners(){
+  slideShow();
+  search();
+}
+
+function search(){
+  $('#search').on('keyup', function(e){
+    console.log(this.value);
+  });
+};
+
+function slideShow(){
+  $('#slideshow').on('click', function(e){
+    e.preventDefault();
+    $('#picture_dropzone').css("display", "none");
+    $('#pictures').slick({
+      slidesToShow: 80,
+      slidesToScroll: 80,
+      centerMode: true,
+      arrows: false,
+      variableWidth: true,
+      // adaptiveHeight: true,
+      dots: false,
+      infinite: true,
+      lazyLoad: 'ondemand',
+      autoplay: true,
+      autoplaySpeed: 3000,
+      fade: true,
+      speed: 500,
+      cssEase: 'linear'
+    });
+  });
+};
