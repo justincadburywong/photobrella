47cccced8be70292d4bf4e0b53bec3314b336d74
Justincadburywong <justincadburywong@gmail.com>
2018-04-21 00:04:27 -0700

    adjust validations

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,8 +1,7 @@
 class Picture < ApplicationRecord
   include PictureUploader[:file]
   belongs_to :user
-  validates :user_id, presence: true, numericality: {only_integer: true, greater_than: 0}
-  validates :file_data, presence: true, uniqueness: true
-  validates :tags, presence: true, uniqueness: true
+  validates :user_id, presence: true
+  validates :file_data, presence: true
   scope :sorted, -> { order(created_at: :desc) }
 end
