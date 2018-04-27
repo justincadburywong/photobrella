eb3b42868c0be9705b646ae87ff8b57a0e96252d
Justin Wong <justincadburywong@gmail.com>
2018-04-27 10:08:04 -0700

    Update pictures_controller.rb

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,5 @@
 class PicturesController < ApplicationController
+  skip_before_filter :verify_authenticity_token
   before_action :authenticate_user!
   before_action :find_picture, only: [:update, :destroy]
 
